88a289b2fe8f79b8b4607d45305c7bc2abb8db65
Eli Belash <elibelash@gmail.com>
2019-09-12 02:39:39 +0300

    Added custom changes, equivalent to the dump file.

--- CalcEventsTFS/Program.cs
+++ CalcEventsTFS/Program.cs
@@ -1,9 +1,11 @@
 ﻿using NumSharp;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using Tensorflow;
 using Tensorflow.Util;
+using System.Runtime.CompilerServices;
 
 namespace CalcEventsTFS
 {
@@ -24,18 +26,16 @@ namespace CalcEventsTFS
             {
                 new Thread(() =>
                 {
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
                     Session sess;
-
-                    lock (Locks.ProcessWide)
-                        sess = Session.LoadFromSavedModel(modelLocation).as_default();
-
+                    Tensor[] inp = null;
+                    Tensor outp = null;
+                    lock (Locks.ProcessWide) sess = LoadFromSavedModel(modelLocation);
+                    var inputs = new[] {"sp", "fuel"};
+                    inp = inputs.Select(name => sess.graph.OperationByName(name).output).ToArray();
+                    outp = sess.graph.OperationByName("softmax_tensor").output;
                     {
-                        var inputs = new[] { "sp", "fuel" };
-
-                        var inp = inputs.Select(name => sess.graph.OperationByName(name).output).ToArray();
-                        var outp = sess.graph.OperationByName("softmax_tensor").output;
-
-                        for (var i = 0; i < 1000; i++)
+                        while (true)
                         {
                             {
                                 var data = new float[96];
@@ -47,16 +47,62 @@ namespace CalcEventsTFS
                                 try
                                 {
                                     sess.run(outp, feeds);
-                                }
-                                catch (Exception ex)
+                                } catch (Exception ex)
                                 {
                                     Console.WriteLine(ex);
+                                    throw;
                                 }
                             }
-                            GC.Collect();
                         }
+
+                        Console.WriteLine($"TID done {Thread.CurrentThread.ManagedThreadId}");
                     }
                 }).Start();
+            }
+
+            Console.WriteLine("Done Starting Threads");
+            Console.ReadLine();
+        }
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public static Session LoadFromSavedModel(string path)
+        {
+            lock (Locks.ProcessWide)
+            {
+                var graph = c_api.TF_NewGraph();
+                var status = new Status();
+                var opt = new SessionOptions();
+
+                var tags = new string[] {"serve"};
+                var buffer = new TF_Buffer();
+
+                IntPtr sess;
+                try
+                {
+                    sess = c_api.TF_LoadSessionFromSavedModel(opt,
+                        IntPtr.Zero,
+                        path,
+                        tags,
+                        tags.Length,
+                        graph,
+                        ref buffer,
+                        status);
+                    status.Check(true);
+                } catch (TensorflowException ex) when (ex.Message.Contains("Could not find SavedModel"))
+                {
+                    status = new Status();
+                    sess = c_api.TF_LoadSessionFromSavedModel(opt,
+                        IntPtr.Zero,
+                        Path.GetFullPath(path),
+                        tags,
+                        tags.Length,
+                        graph,
+                        ref buffer,
+                        status);
+                    status.Check(true);
+                }
+
+                return new Session(sess, g: new Graph(graph)).as_default();
             }
         }
     }
